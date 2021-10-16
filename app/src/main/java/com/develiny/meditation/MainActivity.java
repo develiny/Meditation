88f9d928a2bc8c66230ffa725c474a14225d3c2b
develiny <develiny9@gmail.com>
2021-10-16 21:26:30 +0900

    m1

---
+++ app/src/main/java/com/develiny/meditation/MainActivity.java
@@ -0,0 +1,42 @@
+package com.develiny.meditation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
+public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout bottomSheetTitleBar;
+    BottomSheetBehavior bottomSheetBehavior;
+    LinearLayout linearLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setButtonSheet();
+    }
+
+    private void setButtonSheet() {
+        this.bottomSheetTitleBar = findViewById(R.id.bottom_sheet_title_bar);
+        linearLayout = findViewById(R.id.bottom_sheet_id);
+        bottomSheetBehavior = BottomSheetBehavior.from(linearLayout);
+
+        bottomSheetTitleBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (bottomSheetBehavior.getState() == BottomSheetBehavior.STATE_EXPANDED) {
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                } else if (bottomSheetBehavior.getState() == BottomSheetBehavior.STATE_COLLAPSED) {
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                }
+            }
+        });
+    }
+}
